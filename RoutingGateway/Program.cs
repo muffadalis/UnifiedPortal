eda2914f1d810f058862caac3b22eedadb16b864
muffadalis <muffyj@gmail.com>
2025-07-26 11:28:29 +1200

    clean up

--- RoutingGateway/Program.cs
+++ RoutingGateway/Program.cs
@@ -1,31 +1,4 @@
-﻿//using Microsoft.Extensions.FileProviders;
-//using RoutingGateway;
-
-//var builder = WebApplication.CreateBuilder(args);
-//var app = builder.Build();
-
-//app.UseStaticFiles(); // Serve wwwroot
-
-//// Serve SharedAssets folder statically
-//app.UseStaticFiles(new StaticFileOptions
-//{
-//    FileProvider = new PhysicalFileProvider(
-//        Path.Combine(builder.Environment.ContentRootPath, "SharedAssets")),
-//    RequestPath = "/SharedAssets"
-//});
-
-//// Smart route middleware
-//app.UseMiddleware<SmartRouteMiddleware>();
-
-//// Fallback
-//app.Run(async context =>
-//{
-//    await context.Response.WriteAsync($"Unhandled path: {context.Request.Path}");
-//});
-
-//app.Run();
-
-using Microsoft.Extensions.FileProviders;
+﻿using Microsoft.Extensions.FileProviders;
 
 var builder = WebApplication.CreateBuilder(args);
 
